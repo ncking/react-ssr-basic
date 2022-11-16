00ecd94c2f16598dcea534b91dafb762a6cfae03
Nigel King <nigel.c.king@gmail.com>
2022-11-16 12:11:46 +0000

    fix ssr html return

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import React from 'react';
 import {renderToString} from 'react-dom/server';
 import Home from './component/Home.jsx';
 
-const content = (props) => {
-    renderToString(React.createElement(Home, props));
-}
+const content = (props) => renderToString(React.createElement(Home, props));
+
 
 export default content
